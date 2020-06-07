a540738c96b3c240e0bdc3ba7a42b2c3f92e9c9e
idalmys <idalmysmaza@gmail.com>
2020-06-06 21:17:54 -0500

    Updates

---
+++ precache-manifest.7776d59a4fbb3da67777e9fd938f3a5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b3eaf83a6457db40e8ec9feeaf9b64",
+    "url": "/clicky-game-pages/index.html"
+  },
+  {
+    "revision": "970b2f43b6238d0be144",
+    "url": "/clicky-game-pages/static/css/main.53ceb17c.chunk.css"
+  },
+  {
+    "revision": "55ea0a1c41e3cf21f12c",
+    "url": "/clicky-game-pages/static/js/2.378b37ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game-pages/static/js/2.378b37ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "970b2f43b6238d0be144",
+    "url": "/clicky-game-pages/static/js/main.fa5c46d5.chunk.js"
+  },
+  {
+    "revision": "0317b3e778fd8b993718",
+    "url": "/clicky-game-pages/static/js/runtime-main.198c1d35.js"
+  },
+  {
+    "revision": "529975baddb9367726aa05c3fb70ecfa",
+    "url": "/clicky-game-pages/static/media/fondo2.529975ba.jpg"
+  }
+]);
